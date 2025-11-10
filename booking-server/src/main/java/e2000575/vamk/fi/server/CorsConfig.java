d05a88b52bdda5bff8be501640c177abbea9aad9
hoan301298 <hoantran301298@gmail.com>
2025-11-10 23:47:08 +0200

    Add Allowed Origin from booking server

--- booking-server/src/main/java/e2000575/vamk/fi/server/CorsConfig.java
+++ booking-server/src/main/java/e2000575/vamk/fi/server/CorsConfig.java
@@ -1,5 +1,6 @@
 package e2000575.vamk.fi.server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,14 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
+    @Value("${cors.allowed.origin}")
+    private String allowedOrigin;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-            @SuppressWarnings("null")
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173/")
+                        .allowedOrigins(allowedOrigin)
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
