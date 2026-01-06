7120106d1d690a7a94bab9dfe74975401428b051
hoan301298 <hoantran301298@gmail.com>
2026-01-06 19:10:57 +0200

    remove vite-plugin-react-swc

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react-swc"
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig(({ mode }) => {
@@ -18,16 +18,23 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    plugins: [react()],
+
+    plugins: [
+      react({
+        jsxRuntime: "automatic"
+      })
+    ],
+
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
       },
     },
+
     build: {
-      outDir: 'dist',
+      outDir: "dist",
       sourcemap: false,
-      minify: 'esbuild'
-    },
-  }
+      minify: "esbuild"
+    }
+  };
 });
